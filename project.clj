02f2d019461b74605a538e0b8238da8253696824
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-06-14 20:10:07 +0200

    Do not need parsley.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject scratch "1.0.0-SNAPSHOT"
   :description "a scratch project to rapidly test stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.cgrand.parsley "0.9.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[midje/midje "1.4.0"]])
