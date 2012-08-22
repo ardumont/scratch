4d55b2091aeec59434ef4f0f5130d9fba9a0abdc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-22 19:34:31 +0200

    Change the midje deps from dev deps into a runtime deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject scratch "1.0.0-SNAPSHOT"
   :description "a scratch project to rapidly test stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[midje/midje "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [midje/midje "1.4.0"]]
+  :dev-dependencies [])
