c7fa075072b074e11c11452572d708cc6687bdec
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-29 12:26:02 +0200

    align facts

--- src/scratch/cljprogramming.clj
+++ src/scratch/cljprogramming.clj
@@ -26,7 +26,7 @@
                     (take-nth 2 sequential))))
 
 (fact
-  (swap-pairs (apply list (range 10))) => '(8 9 6 7 4 5 2 3 0 1);; look it's a list!
+  (swap-pairs (apply list (range 10)))   => '(8 9 6 7 4 5 2 3 0 1);; look it's a list!
   (swap-pairs (apply vector (range 10))) => [1 0 3 2 5 4 7 6 9 8]);; it's a vec!
 
 ;; ----------------------------- map-map
@@ -38,7 +38,7 @@
           [k (f v)])))
 
 (fact
-  (map-map inc (hash-map :a 1 :z 10 :c 41)) => (just [:a 2] [:z 11] [:c 42] :in-any-order)
+  (map-map inc (hash-map :a 1 :z 10 :c 41))   => (just [:a 2] [:z 11] [:c 42] :in-any-order)
   (map-map inc (sorted-map :z 10 :c 41 :a 1)) => (sorted-map :z 11 :c 42 :a 2))
 
 (defn map-map2 "Same as before with reduce"
