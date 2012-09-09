921423f944e3f08096fbc277623976766859f108
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-09 10:39:01 +0200

    Simplify.

--- project.clj
+++ project.clj
@@ -1,13 +1,6 @@
 (defproject scratch/scratch "1.0.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [midje/midje "1.4.0"]
+                 [midje "1.4.0"]
                  [org/jaudiotagger "2.0.3"]]
-  :profiles {:dev
-             {:dependencies
-              [[com.intelie/lazytest
-                "1.0.0-SNAPSHOT"
-                :exclusions
-                [swank-clojure]]
-               [midje "1.4.0"]]}}
   :description "a scratch project to rapidly test stuff")
