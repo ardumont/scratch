7855e1912e3729ca1fef2e0fa55886dc01bf5862
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-09-09 10:52:38 +0200

    Play with zipper.

--- src/scratch/zipper.clj
+++ src/scratch/zipper.clj
@@ -5,13 +5,21 @@
 (def v [[1 2 [3 4]] [5 6]])
 ;;= #'scratch.zipper/v
 
-(fact
-  (-> v z/vector-zip z/node) => [[1 2 [3 4]] [5 6]]
-  (-> v z/vector-zip z/down z/node) => [1 2 [3 4]]
-  (-> v z/vector-zip z/down z/down z/node) => 1
-  (-> v z/vector-zip z/down z/down z/right z/node) => 2
-  (-> v z/vector-zip z/down z/right z/node) => [5 6]
-  (-> v z/vector-zip z/down z/right z/down z/right z/node) => 6)
+(fact "Movement on zipper"
+  (let [vz (z/vector-zip v)]
+    (-> vz z/node)                               => [[1 2 [3 4]] [5 6]]
+    (-> vz z/down z/node)                        => [1 2 [3 4]]
+    (-> vz z/down z/down z/node)                 => 1
+    (-> vz z/down z/down z/right z/node)         => 2
+    (-> vz z/down z/right z/node)                => [5 6]
+    (-> vz z/down z/right z/down z/right z/node) => 6))
+
+(fact "Editing zipper"
+  (let [vz (z/vector-zip v)]
+    (-> vz z/down z/right (z/replace 56) z/root) => [[1 2 [3 4]] 56]
+    (-> vz z/down z/right (z/remove) z/node)     => 4
+    (-> vz z/down z/right (z/remove) z/root)     => [[1 2 [3 4]]]
+    (-> vz z/down z/right (z/replace 56) (z/edit * 2) z/root) => [[1 2 [3 4]] 112]))
 
 
 
