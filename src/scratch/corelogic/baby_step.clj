fa8930627ca0861c19d7b5f84f3b3f759f303b5c
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-09 12:46:02 +0100

    Some play around membero, conde and ==

--- src/scratch/corelogic/baby_step.clj
+++ src/scratch/corelogic/baby_step.clj
@@ -8,4 +8,36 @@
   (run* [q]
         (== true q))
   ;; (true)
+
+  ;; membero, give me all the q that satisfies the relation 'q is a member of the list '(:a :b :c)
+  (run* [q]
+        (membero q '(:a :b :c)))
+  ;; '(:a :b :c)
+
+  ;; some logical intersection
+  (run* [q]
+        (membero q '(:a :b :c))
+        (membero q '(:b :c :d)))
+  ;; '(:b :c)
+
+  ;; this can also be written with the fresh and unify operations
+  (run* [q]
+        (fresh [m]
+               (membero m '(:a :b :c))
+               (membero q '(:b :c :d))
+               (== m q)))
+  ;; '(:b :c)
+
+  ;; now for some logical union
+  (run* [q]
+        (conde
+         ((membero q '(:a :b :c)))
+         ((membero q '(:b :c :d)))))
+  ;; '(:a :b :b :c :c :d)
+
+  (run* [q]
+        (conde
+         [(membero q '(:a :b :c))]
+         [(membero q '(:b :c :d))]))
+  ;; '(:a :b :b :c :c :d)
   )
