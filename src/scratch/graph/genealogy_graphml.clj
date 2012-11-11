09bf0bedf89040280257cfa9cf4e7e12a4025ac6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-11 19:51:57 +0100

    make-graphml

--- src/scratch/graph/genealogy_graphml.clj
+++ src/scratch/graph/genealogy_graphml.clj
@@ -90,3 +90,39 @@
                        {:id :e1
                         :source :n2
                         :target :n3}]}))
+
+(defn make-graphml-headers
+  "Generate the standard graphml header."
+  []
+  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\"
+    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+    xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns
+     http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd\">")
+
+(comment
+  (make-graphml-headers))
+
+(defn make-graphml
+  "Compute the overall graphml file."
+  [graphs]
+  (s/join "\n"
+          [(make-graphml-headers)
+           (->> graphs
+                (map make-graph)
+                (s/join "\n"))
+           "</graphml>"]))
+
+(comment
+  (make-graphml [{:id :some-graph
+                   :edgedefault :directed
+                   :nodes [{:id :n1}
+                           {:id :n2}
+                           {:id :n3}
+                           {:id :n4}]
+                   :edges [{:id :e1
+                            :source :n1
+                            :target :n3}
+                           {:id :e1
+                            :source :n2
+                            :target :n3}]}]))
