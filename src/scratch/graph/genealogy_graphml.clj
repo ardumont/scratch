4c403758fbb037111a7d96928e0ee46f9ad939cc
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-11 19:36:03 +0100

    proposed structure.

--- src/scratch/graph/genealogy_graphml.clj
+++ src/scratch/graph/genealogy_graphml.clj
@@ -13,3 +13,16 @@
 ;; <edge id='e2' source='n2' target='n3' />
 ;; </graph>
 ;; </graphml>
+
+;; proposed structure
+{:edgedefault :directed
+ :nodes [{:id :n1}
+         {:id :n2}
+         {:id :n3}
+         {:id :n4}]
+ :edges [{:id :e1
+          :source :n1
+          :target :n3}
+         {:id :e1
+          :source :n2
+          :target :n3}]}
