7302d61202f1377747eb7f74cbba83ce28e6c4a1
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-11 20:16:00 +0100

    compute the nodes in the graphml namespace format.

--- src/scratch/graph/genealogy_graphml.clj
+++ src/scratch/graph/genealogy_graphml.clj
@@ -1,4 +1,15 @@
 (ns scratch.graph.genealogy-graphml
   (:require [clojure.string :as s]
             [scratch.graph.genealogy :as g]
-            [scratch.graph.graphml :as g]))
+            [scratch.graph.graphml :as gml]))
+
+(defn make-nodes
+  "Compute the nodes in the graphml namespace format."
+  [nodes]
+  (->> nodes
+       flatten
+       (map keyword)
+       set
+       (map (fn [n] {:id (keyword n)}))))
+
+(make-nodes (g/parents))
