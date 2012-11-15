66f8a66f2157311a0a32b349eb21135747fd7753
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-15 20:00:34 +0100

    Improve the genealogy graph to make the node colourful depending on the gender

--- src/scratch/graph/genealogy_dorothy.clj
+++ src/scratch/graph/genealogy_dorothy.clj
@@ -8,10 +8,12 @@
   "Given a list of edges, compute the nodes"
   [edges]
   (->> edges
-       flatten
        (map keyword)
        set
        vec))
+
+(add-nodes (g/females))
+(add-nodes (g/males))
 
 (defn add-edges
   "Compute the edges"
@@ -20,25 +22,37 @@
        (map (fn [v] (map keyword v)))
        (map vec)))
 
-(let [mothers (g/mothers)
-      fathers (g/fathers)
-      parents (concat mothers fathers)]
-  (-> (concat
-       ;; shape
-       ;; [[:node {:shape :box}]]
-       ;; all the nodes
-       (add-nodes parents)
-       ;; all the mothers
-       (add-edges mothers)
-       ;; all the fathers
-       (add-edges fathers))
-      doall
-      vec
-      digraph
-      dot
-      ;; render a svg format
-      ;;    (render {:format :svg})
-      ;;    (render {:format :png})
-      ;; persist the file
-      ;;    (save! "out.png" {:format :png})
-      (show!)))
+(-> (concat
+     ;; shape
+     ;; [[:node {:shape :box}]]
+
+     ;; all the nodes
+
+     ;; mothers
+     [(node-attrs {:style :filled
+                   :color :pink
+                   :shape :ellipse})]
+     (add-nodes (g/females))
+
+     ;; fathers
+     [(node-attrs {:style :filled
+                   :color :cyan})]
+     (add-nodes (g/males))
+
+     ;; all the mothers
+     (add-edges (g/mothers))
+
+     ;; all the fathers
+     (add-edges (g/fathers)))
+    doall
+    vec
+    digraph
+    dot
+    ;; render a svg format
+    ;; (render {:format :svg})
+    ;;    (render {:format :png})
+    ;; persist the file
+    ;;    (save! "out.png" {:format :png})
+    ;;    (show!)
+
+    (save! "genealogy-family-christelle-antoine.svg" {:format :svg}))
