578d724fc99cb66914dec0f76cf076c6e3a658b5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-15 21:53:26 +0100

    Compute and render as pretty as possible the relation between couples

--- src/scratch/graph/genealogy_dorothy.clj
+++ src/scratch/graph/genealogy_dorothy.clj
@@ -4,6 +4,27 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; dorothy (graphviz generation) play
 
+(defn make-procreate-edge
+  []
+  (let [procreate (g/procreate)]
+    (concat
+     ;; compute the links [father-mother child]
+     (->> procreate
+          (mapcat (fn [[m f :as v]]
+                    (let [common (keyword (format "%s-%s" m f))]
+                      (->> v
+                           (mapcat g/children)
+                           (map (fn [c] [common (keyword c)]))))))
+          set
+          vec)
+     ;; compute the links [[father father-mother] [mother father-mother]]
+     (->> procreate
+          (mapcat (fn [[m f :as v]]
+                    (let [common (keyword (format "%s-%s" m f))]
+                      [[(keyword m) common] [(keyword f) common]])))))))
+(comment
+  (make-procreate-edge))
+
 (defn add-nodes
   "Given a list of edges, compute the nodes"
   [edges]
@@ -12,8 +33,9 @@
        set
        vec))
 
-(add-nodes (g/females))
-(add-nodes (g/males))
+(comment
+  (add-nodes (g/females))
+  (add-nodes (g/males)))
 
 (defn add-edges
   "Compute the edges"
@@ -22,39 +44,47 @@
        (map (fn [v] (map keyword v)))
        (map vec)))
 
-(-> (concat
-     ;; shape
-     ;; [[:node {:shape :box}]]
+(comment
+  (-> (concat
+       ;; shape
+       ;; [[:node {:shape :box}]]
 
-     ;; all the nodes
+       ;; all the nodes
 
-     ;; mothers
-     [(node-attrs {:style :filled
-                   :color :pink
-                   :shape :ellipse})]
-     (add-nodes (g/females))
+       ;; mothers
+       [(node-attrs {:style :filled
+                     :color :pink
+                     :shape :ellipse})]
+       (add-nodes (g/females))
 
-     ;; fathers
-     [(node-attrs {:style :filled
-                   :color :cyan})]
-     (add-nodes (g/males))
+       ;; fathers
+       [(node-attrs {:style :filled
+                     :color :cyan})]
+       (add-nodes (g/males))
 
-     ;; parents
-     (add-edges (g/parents))
+       [(node-attrs {:style :filled
+                     :color :purple
+                     :height "0"
+                     :width "0"
+                     :label ""})]
+       (->> (make-procreate-edge)
+            (map first)
+            add-nodes)
 
-     [(edge-attrs {:arrowhead :none :color :red :label "&"})]
-     ;; the couples that add children
-     (add-edges (g/procreate)))
+       [(edge-attrs {:arrowhead :none
+                     :color :red})]
+       ;; the couples that add children
+       (add-edges (make-procreate-edge)))
 
-    doall
-    vec
-    digraph
-    dot
-    ;; render a svg format
-    ;; (render {:format :svg})
-    ;;    (render {:format :png})
-    ;; persist the file
-    ;;    (save! "out.png" {:format :png})
-    ;;    (show!)
+      doall
+      vec
+      digraph
+      dot
+      ;; render a svg format
+      ;; (render {:format :svg})
+      ;;    (render {:format :png})
+      ;; persist the file
+      ;;    (save! "out.png" {:format :png})
+      ;;    (show!)
 
-    (save! "genealogy-family-christelle-antoine.svg" {:format :svg}))
+      (save! "genealogy-family-christelle-antoine.svg" {:format :svg})))
