82441761c460e8ff356b97ab1362d4fcbcc9becf
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-25 16:38:40 +0100

    Progress on listing the files with the drive api. Now 401.

--- src/scratch/drive/api.clj
+++ src/scratch/drive/api.clj
@@ -4,82 +4,101 @@
             [oauth.google      :as oauth]))
 
 ;; your credentials in the ~/.sdk-drive/config.clj file
-;; (def sdk-drive-credentials
+;; (def drive-credentials
 ;;   {:api-key        ""
 ;;    :client-id      ""
 ;;    :client-secret  ""
-;;    :redirect-uri   ""})
+;;    :redirect-uri   ""
+;;    :code           ""});; provided by the authorization entry in the browser
 
 (load-file (str (System/getProperty "user.home") "/.sdk-drive/config.clj"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; authentication
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; authorization utility functions
+
+(def ^{:doc "The google api urls for authorisation."
+       :private true}
+  url-api-auth "https://www.googleapis.com/auth")
+
+(defn- url-api-authorization
+  "Factor the code to compute the url needed for authorization"
+  [s]
+  (str url-api-auth s))
+
+;; full list of scopes for the google drive api: https://developers.google.com/drive/scopes
+(def ^{:doc "The scope we authorise the application to access."
+       :private true}
+  scopes
+  {:drive            (url-api-authorization "/drive")})
+
+;; :drive-readonly   (url-api-authorization "/drive.readonly")
+;; :email            (url-api-authorization "/userinfo.email")
+;; :profile          (url-api-authorization "/userinfo.profile")
+
+(defn- get-scopes
+  "Compute the scopes authorization entries"
+  []
+  (s/join " " (vals scopes)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; use case
 
 (comment
-  ;; compute the authorization url
-  (oauth/oauth-authorization-url (:client-id sdk-drive-credentials)
-                                 (:redirect-uri sdk-drive-credentials))
+  ;; authentication
+  (oauth/oauth-authorization-url (:client-id drive-credentials)
+                                 (:redirect-uri drive-credentials)
+                                 :scope (get-scopes))
 
-  (comment
+  ;; ****************** manual intervention here:
+  ;; this needs to be executed to ask for the permission for the application
+  ;; the browser will open and ask for permission
+  ;; once accepted, you will be prompted with a code
+  ;; i added the code in question in the map in the drive-credentials
+  (oauth/oauth-authorize (:client-id    drive-credentials)
+                         (:redirect-uri drive-credentials)
+                         :scope (get-scopes))
 
-    ;; ****************** manual intervention here:
-    ;; this needs to be executed to ask for the permission for the application
-    ;; the browser will open and ask for permission
-    ;; once accepted, you will be prompted with a code
-    ;; i added the code in question in the map in the sdk-drive-credentials
-    (oauth/oauth-authorize (:client-id sdk-drive-credentials)
-                           (:redirect-uri sdk-drive-credentials))
+  ;; obtain the oauth access from google
+  (def oauth-access-token-app
+    (oauth/oauth-access-token (:client-id     drive-credentials)
+                              (:client-secret drive-credentials)
+                              authorization-code
+                              (:redirect-uri  drive-credentials))))
 
-    ;; obtain the oauth access from google
-    (def oauth-access-token-app
-      (oauth/oauth-access-token (:client-id     sdk-drive-credentials)
-                                (:client-secret sdk-drive-credentials)
-                                (:code          sdk-drive-credentials)
-                                (:redirect-uri  sdk-drive-credentials)))
-    ;; format of the token
-    ;;    {:id-token "some-token",
-    ;;     :expires-in 3600,
-    ;;     :token-type "Bearer",
-    ;;     :refresh-token "some-refresh-token",
-    ;;     :access-token "some-access-token"}
-
-    (def oauth-client-app (oauth/oauth-client (oauth-access-token-app :access-token)))
-
-    (oauth/user-info oauth-client-app)
-    ;; {:gender         "male",
-    ;;  :link           "https://plus.google.com/some-id",
-    ;;  :name           "Antoine R. Dumont",
-    ;;  :given-name     "Antoine R.",
-    ;;  :locale         "en",
-    ;;  :verified-email true,
-    ;;  :family-name    "Dumont",
-    ;;  :email          "some-email@some-provider",
-    ;;  :id             "some-id",
-    ;;  :birthday       "some-birthday",
-    ;;  :picture        "some-uri-pointing-to-his-her-picture"}
-    ))
-
-
-(defn url
-  "drive api url"
-  []
-  "https://www.googleapis.com/drive/v2")
+(def ^{:doc "Reference the possibles api url"} url-api
+  {:drive "https://www.googleapis.com/drive/v2"})
 
 (defn compute-url
   "Compute url with authentication needed."
   [url path]
   (format "%s%s" url path))
 
+(comment
+  (compute-url (:drive url-api) "/files"))
+
 (defn api-query
   [method path & [opts]]
   (c/request
    (merge {:method     method
-           :url        (compute-url (url) path)
+           :url        (compute-url (:drive url-api) path)
            :accept     :json
            :as         :json
-           :oauth-token (oauth-access-token-app :access-token)}
+           :oauth-token (oauth-access-token-app :access-token)
+           :headers    {"Authorization" "Bearer"}}
           opts)))
 
-(api-query :get "/files")
-;; 403 - the query is ok but i do not have permissions to access such content
-;; 403 Forbidden
-;; The request was a valid request, but the server is refusing to respond to it.[2] Unlike a 401 Unauthorized response, authenticating will make no difference.[2] On servers where authentication is required, this commonly means that the provided credentials were successfully authenticated but that the credentials still do not grant the client permission to access the resource (e.g. a recognized user attempting to access restricted content).
+(comment
+  (api-query :get "/files")
+  ;; {
+  ;;  "error": {
+  ;;   "errors": [
+  ;;    {
+  ;;     "domain": "usageLimits",
+  ;;     "reason": "dailyLimitExceededUnreg",
+  ;;     "message": "Daily Limit for Unauthenticated Use Exceeded. Continued use requires signup.",
+  ;;     "extendedHelp": "https://code.google.com/apis/console"
+  ;;    }
+  ;;   ],
+  ;;   "code": 403,
+  ;;   "message": "Daily Limit for Unauthenticated Use Exceeded. Continued use requires signup."
+  ;;  }
+  ;; }
+)
